813142ae103195b0e935f4a1e6936501e50c05e2
Kristian Dupont <kristian@kristiandupont.com>
2022-08-15 06:23:51 +0000

    Update example

--- example/.kanelrc.js
+++ example/.kanelrc.js
@@ -1,12 +1,11 @@
-const path = require('path');
+const { join } = require('path');
 const { recase } = require('@kristiandupont/recase');
+const { tryParse } = require('tagged-comment-parser')
 
-// This hook will insert the name of the model or type right after the auto-generated warning comment.
-const insertNameComment = (lines, src) => {
-  const [h1, h2, ...tail] = lines;
-  return [h1, h2, `// Name: ${src.name}`, ...tail];
-};
+const toPascalCase = recase('snake', 'pascal');
+const outputPath = './example/models';
 
+/** @type {import('../src/Config').default} */
 module.exports = {
   connection: {
     host: 'localhost',
@@ -17,26 +16,53 @@ module.exports = {
     port: 54321,
   },
 
-  modelNominator: recase('snake', 'pascal'),
-  typeNominator: recase('snake', 'pascal'),
-  preDeleteModelFolder: true,
+  outputPath,
+  resolveViews: true,
+  preDeleteOutputFolder: true,
 
-  customTypeMap: {
-    // There is no such package, this is just an example. See Film.ts to see the result.
-    tsvector: { name: 'TsVector', module: 'ts-vector', absoluteImport: true, defaultImport: true },
-    bpchar: 'string',
+  getMetadata: (details, generateFor) => {
+    const { comment: strippedComment } = tryParse(details.comment);
+    const isAgentNoun = ['initializer', 'mutator'].includes(generateFor);
+
+    const relationComment = isAgentNoun
+      ? `Represents the ${generateFor} for the ${details.kind} ${details.schemaName}.${details.name}`
+      : `Represents the ${details.kind} ${details.schemaName}.${details.name}`;
+
+    const suffix = isAgentNoun ? `_${generateFor}` : '';
+
+    return {
+      name: toPascalCase(details.name + suffix),
+      comment: [relationComment, ...(strippedComment ? [strippedComment] : [])],
+      path: join(outputPath, toPascalCase(details.name)),
+    };
   },
 
-  modelHooks: [insertNameComment],
-  typeHooks: [insertNameComment],
+  generateIdentifierType: (c, d) => {
+    // Id columns are already prepended with the table name, so we don't need to add it here
+    const name = toPascalCase(c.name);
 
-  resolveViews: true,
+    // Identifiers in the example database are all int4's.
+    const innerType = 'number'; 
+    
+    return {
+      declarationType: 'typeDeclaration',
+      name,
+      exportAs: 'named',
+      typeDefinition: [`${innerType} & { __flavor?: '${name}' }`],
+      comment: [`Identifier type for ${d.name}`],
+    };
+  },
 
-  schemas: [
-    {
-      name: 'public',
-      modelFolder: path.join(__dirname, 'models'),
-      ignore: ['film_list', 'staff'],
-    },
-  ],
+  customTypeMap: {
+    // A text search vector could be stored as a set of strings. See Film.ts for an example.
+    'pg_catalog.tsvector': 'Set<string>',
+
+    // The bytea package (https://www.npmjs.com/package/postgres-bytea) could be used for byte arrays.
+    // See Staff.ts for an example.
+    'pg_catalog.bytea': { name: 'bytea', path: 'bytea', isAbsolute: true, isDefault: true },
+
+    // Columns with the following types would probably just be strings in TypeScript.
+    'pg_catalog.bpchar': 'string',
+    'public.citext': 'string'
+  },
 };
