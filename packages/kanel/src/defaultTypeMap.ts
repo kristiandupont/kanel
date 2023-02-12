2b22784e564a8311b1d2523ec1a181973af51702
Pierre Dahmani <git@pierrefhapp.dev>
2023-02-12 09:51:00 +0100

    switch default type of int8, float8 and numeric to typescript type number.

--- packages/kanel/src/defaultTypeMap.ts
+++ packages/kanel/src/defaultTypeMap.ts
@@ -3,10 +3,10 @@ import TypeMap from './TypeMap';
 const defaultTypeMap: TypeMap = {
   'pg_catalog.int2': 'number',
   'pg_catalog.int4': 'number',
-  'pg_catalog.int8': 'string',
+  'pg_catalog.int8': 'number',
   'pg_catalog.float4': 'number',
-  'pg_catalog.float8': 'string',
-  'pg_catalog.numeric': 'string',
+  'pg_catalog.float8': 'number',
+  'pg_catalog.numeric': 'number',
   'pg_catalog.bool': 'boolean',
   'pg_catalog.json': 'unknown',
   'pg_catalog.jsonb': 'unknown',
