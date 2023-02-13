4d43975886870b60b14450a8da3b593aed1a6638
Kristian Dupont <kristian@kristiandupont.com>
2023-02-13 15:26:17 +0000

    Fix float8 default type and document int8

--- packages/kanel/src/defaultTypeMap.ts
+++ packages/kanel/src/defaultTypeMap.ts
@@ -3,9 +3,15 @@ import TypeMap from './TypeMap';
 const defaultTypeMap: TypeMap = {
   'pg_catalog.int2': 'number',
   'pg_catalog.int4': 'number',
+
+  // JS numbers are always floating point, so there is only 53 bits of precision
+  // for the integer part. Thus, storing a 64-bit integer in a JS number will
+  // result in potential data loss. We therefore use strings for 64-bit integers
+  // the same way that the pg driver does.
   'pg_catalog.int8': 'string',
+
   'pg_catalog.float4': 'number',
-  'pg_catalog.float8': 'string',
+  'pg_catalog.float8': 'number',
   'pg_catalog.numeric': 'string',
   'pg_catalog.bool': 'boolean',
   'pg_catalog.json': 'unknown',
