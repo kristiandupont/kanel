68542069334e58123d355281a96e1b52bd8dd107
Kristian Dupont <kristian@kristiandupont.com>
2023-04-30 06:29:34 +0000

    Expose composite-types

--- packages/kanel/src/index.ts
+++ packages/kanel/src/index.ts
@@ -3,6 +3,10 @@ export * from './declaration-types';
 export * from './default-metadata-generators';
 export { default as Details } from './Details';
 export { default as escapeName } from './escapeName';
+export {
+  CompositeDetails,
+  CompositeProperty,
+} from './generators/composite-types';
 export { default as resolveType } from './generators/resolveType';
 export * from './hooks';
 export * from './metadata-types';
