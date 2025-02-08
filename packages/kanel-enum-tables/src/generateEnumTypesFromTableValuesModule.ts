5b258229816508d848b41dd789201a60f4e425a5
Simon Fridlund <simon@fridlund.email>
2025-02-08 10:30:19 +0100

    Update code according to kanel-knex

--- packages/kanel-enum-tables/src/generateEnumTypesFromTableValuesModule.ts
+++ packages/kanel-enum-tables/src/generateEnumTypesFromTableValuesModule.ts
@@ -4,36 +4,36 @@ import type {
   Output,
   PreRenderHook,
   TypeDeclaration,
-} from 'kanel';
-import { escapeName } from 'kanel';
-import knex from 'knex';
+} from "kanel";
+import { escapeName } from "kanel";
+import knex from "knex";
 
 const generateEnumTypesFromTableValuesModule: PreRenderHook = async (
   outputAccumulator,
-  instantiatedConfig
+  instantiatedConfig,
 ) => {
   const connection = instantiatedConfig.connection;
-  const db = knex({ client: 'postgres', connection });
+  const db = knex({ client: "postgres", connection });
 
   // Get all the tables in all the schemas
   const allTables = Object.values(instantiatedConfig.schemas).flatMap(
-    ({ tables }) => tables
+    ({ tables }) => tables,
   );
   // ..and find the ones that have an "@enum" comment.
   const enumTables = allTables.filter(
-    (table) => table.comment !== null && table.comment.indexOf('@enum') !== -1
+    (table) => table.comment !== null && table.comment.indexOf("@enum") !== -1,
   );
 
   const overrides: Output = {};
 
   for (const table of enumTables) {
     const primaryKeyColumns = table.columns.filter(
-      (column) => column.isPrimaryKey
+      (column) => column.isPrimaryKey,
     );
 
     if (primaryKeyColumns.length !== 1) {
       throw new Error(
-        `Enum table ${table.schemaName}.${table.name} must have exactly one primary key column`
+        `Enum table ${table.schemaName}.${table.name} must have exactly one primary key column`,
       );
     }
 
@@ -42,32 +42,32 @@ const generateEnumTypesFromTableValuesModule: PreRenderHook = async (
     const primaryKeyTypeDeclaration = instantiatedConfig.generateIdentifierType(
       primaryKeyColumn,
       table,
-      instantiatedConfig
+      instantiatedConfig,
     );
 
     const selectorMetadata = instantiatedConfig.getMetadata(
       table,
-      'selector',
-      instantiatedConfig
+      "selector",
+      instantiatedConfig,
     );
 
     const initializerMetadata = instantiatedConfig.getMetadata(
       table,
-      'initializer',
-      instantiatedConfig
+      "initializer",
+      instantiatedConfig,
     );
 
     const mutatorMetadata = instantiatedConfig.getMetadata(
       table,
-      'mutator',
-      instantiatedConfig
+      "mutator",
+      instantiatedConfig,
     );
 
     const declarations =
       outputAccumulator[selectorMetadata.path]?.declarations ?? [];
 
     // TODO: Get @enumName and use it as the name of the enum type
-    // TODO: Get @enumDescription (default "descritpion") and use it as the comment for the enum type
+    // TODO: Get @enumDescription (default "description") and use it as the comment for the enum type
 
     const rows = await db
       .withSchema(table.schemaName)
@@ -77,14 +77,14 @@ const generateEnumTypesFromTableValuesModule: PreRenderHook = async (
 
     const newDeclarations = declarations.map((declaration) => {
       if (
-        declaration.declarationType === 'typeDeclaration' &&
+        declaration.declarationType === "typeDeclaration" &&
         declaration.name === primaryKeyTypeDeclaration.name
       ) {
-        if (instantiatedConfig.enumStyle === 'type') {
+        if (instantiatedConfig.enumStyle === "type") {
           const newDeclaration: TypeDeclaration = {
             ...declaration,
             typeDefinition: [
-              '', // Start definition on new line
+              "", // Start definition on new line
               ...rows.map((row) => `| '${row.name}'`),
             ],
           };
@@ -92,31 +92,31 @@ const generateEnumTypesFromTableValuesModule: PreRenderHook = async (
           return newDeclaration;
         } else {
           const newDeclaration: GenericDeclaration = {
-            declarationType: 'generic',
+            declarationType: "generic",
             comment: declaration.comment,
             lines: [
               `export enum ${declaration.name} {`,
               ...rows.map(
-                (row) => `  ${escapeName(row.name)} = '${row.name}',`
+                (row) => `  ${escapeName(row.name)} = '${row.name}',`,
               ),
-              '};',
+              "};",
             ],
           };
 
           return newDeclaration;
         }
       } else if (
-        declaration.declarationType === 'interface' &&
+        declaration.declarationType === "interface" &&
         (declaration.name === initializerMetadata.name ||
           declaration.name === mutatorMetadata.name)
       ) {
         const newDeclaration: InterfaceDeclaration = {
           ...declaration,
           properties: declaration.properties.map((property) => {
-            if (property.name === 'name') {
+            if (property.name === "name") {
               return {
                 ...property,
-                typeName: 'string',
+                typeName: "string",
                 typeImports: [],
               };
             }
