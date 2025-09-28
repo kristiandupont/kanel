4462a61b875150394e1df68f26f994514c92f7a6
Kristian Dupont <kristian@kristiandupont.com>
2025-09-28 19:40:08 +0000

    Fix linter issue

--- packages/kanel/src/index.ts
+++ packages/kanel/src/index.ts
@@ -1,13 +1,6 @@
 export type * from "./config-types";
-export type * from "./ts-utilities/ts-declaration-types";
 export * from "./default-metadata-generators";
 export type { default as Details } from "./Details";
-export { default as escapeComment } from "./ts-utilities/escapeComment";
-export { default as escapeFieldName } from "./ts-utilities/escapeFieldName";
-// For backwards compatibility
-export { default as escapeName } from "./ts-utilities/escapeFieldName";
-export { default as escapeIdentifier } from "./ts-utilities/escapeIdentifier";
-export { default as escapeString } from "./ts-utilities/escapeString";
 export type {
   CompositeDetails,
   CompositeProperty,
@@ -17,6 +10,12 @@ export * from "./hooks";
 export type * from "./metadata-types";
 export type { FileContents, default as Output, Path } from "./Output";
 export { default as processDatabase } from "./processDatabase";
+export { default as escapeComment } from "./ts-utilities/escapeComment";
+export { default as escapeFieldName } from "./ts-utilities/escapeFieldName";
+export { default as escapeName } from "./ts-utilities/escapeFieldName";
+export { default as escapeIdentifier } from "./ts-utilities/escapeIdentifier";
+export { default as escapeString } from "./ts-utilities/escapeString";
+export type * from "./ts-utilities/ts-declaration-types";
 export type { default as TypeDefinition } from "./ts-utilities/TypeDefinition";
 export type { default as TypeImport } from "./ts-utilities/TypeImport";
 export type { default as TypeMap } from "./TypeMap";
